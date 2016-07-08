85028f735d013c10b4b27c035f6e8f1eb1ebb15a
yan <yan@mit.edu>
2016-07-08 10:25:59 -0700

    python dict values() is not ordered

--- emojiprint.py
+++ emojiprint.py
@@ -1,14 +1,14 @@
-from codes import EMOJI_UNICODE
+from codes import EMOJI_UNICODE_VALUES
 
 #print len(EMOJI_UNICODE)
-vals = EMOJI_UNICODE.values()
+vals = EMOJI_UNICODE_VALUES
 hexstring = "A65EB6C09CE4632F2D849F9CF81BF54B83771CDA"
 
 #v = reduce(lambda x, y: (x<<4) + y, map(lambda x: int(x, 16), hexstring))
 #print v
 
 v = int(hexstring, 16)
-mod = len(EMOJI_UNICODE)
+mod = len(EMOJI_UNICODE_VALUES)
 
 out = u""
 while v > 0:
